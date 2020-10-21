24daecb34dfcefd277717d7e7d4fd3449a6ecf2f
kwjcheng88 <43528899+kwjcheng88@users.noreply.github.com>
2020-10-21 10:10:51 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
 })
